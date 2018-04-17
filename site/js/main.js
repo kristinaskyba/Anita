c857647008ee133875de9efc26422d735c1b39d1
Khrystyna Skyba <Tina@Tina-2.local>
2018-04-17 18:14:23 +0300

    1

---
+++ site/js/main.js
@@ -0,0 +1,6 @@
+
+                $(function() {
+                  $('#disableTimeRangesExample').timepicker({ 'disableTimeRanges': [['1am', '2am'], ['3am', '4:01am']] });
+              });
+          
+             
